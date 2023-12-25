182491d908d3ca2b93fbd4a6716b00b22fd43cde
abdemirza <abdemirza@gmail.com>
2023-12-25 17:22:33 +0530

    feat: location permission support

--- android/app/src/main/java/com/stantech_assignment/BatteryStatusModule.java
+++ android/app/src/main/java/com/stantech_assignment/BatteryStatusModule.java
@@ -2,10 +2,16 @@
 
 package com.stantech_assignment;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.PowerManager;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
@@ -13,6 +19,8 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 public class BatteryStatusModule extends ReactContextBaseJavaModule {
+
+  final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
   public BatteryStatusModule(ReactApplicationContext reactContext) {
     super(reactContext);
@@ -37,6 +45,25 @@ public class BatteryStatusModule extends ReactContextBaseJavaModule {
       promise.reject(
         "UNSUPPORTED_ANDROID_VERSION",
         "This feature requires Android Lollipop MR1 or higher"
+      );
+    }
+  }
+
+  private boolean checkLocationPermission() {
+    int permissionResult = ContextCompat.checkSelfPermission(
+      getReactApplicationContext(),
+      Manifest.permission.ACCESS_FINE_LOCATION
+    );
+    return permissionResult == PackageManager.PERMISSION_GRANTED;
+  }
+
+  @ReactMethod
+  public void requestLocationPermission() {
+    if (!checkLocationPermission()) {
+      ActivityCompat.requestPermissions(
+        getCurrentActivity(),
+        new String[] { Manifest.permission.ACCESS_FINE_LOCATION },
+        LOCATION_PERMISSION_REQUEST_CODE
       );
     }
   }
